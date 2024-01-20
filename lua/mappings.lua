72a7ff9c4178967c2f323b8566601fd9292b079c
Dillon Townsend <townsenddillon.dt@gmail.com>
2024-01-20 06:28:41 -0600

    yeah

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,4 +3,6 @@ vim.wo.signcolumn = 'yes'
 vim.g.mapleader = ' '
 vim.keymap.set('n', '<C-l>', '<C-w>l', {})
 vim.keymap.set('n', '<C-h>', '<C-w>h', {})
+vim.keymap.set('n', '<C-j>', '<C-w>j', {})
+vim.keymap.set('n', '<C-k>', '<C-w>k', {})
 
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,18 +7,5 @@ return {
 			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 			vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
 		end
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require('telescope').setup({
-				extensions = {
-					['ui-select'] = {
-						require('telescope.themes').get_dropdown {}
-					}
-				}
-			})
-			require("telescope").load_extension("ui-select")
-		end
 	}
 }
