098a25129f5e9d83a12c1a94bfba0ae22eebfe51
Dillon Townsend <townsenddillon.dt@gmail.com>
2024-01-20 06:59:48 -0600

    added none-ls

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,6 +1,9 @@
 vim.opt.relativenumber = true
+
 vim.wo.signcolumn = 'yes'
+
 vim.g.mapleader = ' '
+
 vim.keymap.set('n', '<C-l>', '<C-w>l', {})
 vim.keymap.set('n', '<C-h>', '<C-w>h', {})
 vim.keymap.set('n', '<C-j>', '<C-w>j', {})
