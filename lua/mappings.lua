36af01275de38a71a010177c9f066caeecd69276
Dillon Townsend <townsenddillon.dt@gmail.com>
2024-01-20 09:54:49 -0600

    mappings

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,11 +1,32 @@
 vim.opt.relativenumber = true
-
 vim.wo.signcolumn = 'yes'
-
 vim.g.mapleader = ' '
 
+
+-- switch between windows
 vim.keymap.set('n', '<C-l>', '<C-w>l', {})
 vim.keymap.set('n', '<C-h>', '<C-w>h', {})
 vim.keymap.set('n', '<C-j>', '<C-w>j', {})
 vim.keymap.set('n', '<C-k>', '<C-w>k', {})
 
+
+-- Allow moving the cursor through wrapped lines with j, k, <Up> and <Down>
+vim.keymap.set('n', 'j', 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', { expr = true })
+vim.keymap.set('n', 'k', 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', { expr = true })
+vim.keymap.set('x', 'j', 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', { expr = true })
+vim.keymap.set('x', 'k', 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', { expr = true })
+
+
+-- escape terminal mode
+vim.keymap.set('t', '<C-x>', vim.api.nvim_replace_termcodes("<C-\\><C-N>", true, true, true), {})
+
+
+-- Don't copy the replaced text after pasting in visual mode
+vim.keymap.set('x', 'p', 'p:let @+=@0<CR>:let @"=@0<CR>', { silent = true })
+
+
+-- center when moving
+vim.keymap.set('n', '<C-d>', '<C-d>zz', {})
+vim.keymap.set('n', '<C-u>', '<C-u>zz', {})
+vim.keymap.set('n', 'n', 'nzz', {})
+vim.keymap.set('n', 'N', 'Nzz', {})
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,11 +1,15 @@
 return {
 	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.5',
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.5',
 		dependencies = { 'nvim-lua/plenary.nvim' },
 		config = function()
 			local builtin = require('telescope.builtin')
 			vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 			vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
+			vim.keymap.set('n', '<leader>fo', '<cmd> Telescope oldfiles <CR>', {})
+			vim.keymap.set('n', '<leader>fa', '<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>', {})
+			vim.keymap.set('n', '<leader>fb', '<cmd> Telescope buffers <CR>', {})
 		end
 	}
 }
