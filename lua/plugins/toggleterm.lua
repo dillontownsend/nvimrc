bd3b5200d647f47723c4781c7ac73a246e275c77
Dillon Townsend <townsenddillon.dt@gmail.com>
2024-01-20 10:02:49 -0600

    added toggleterm

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,10 @@
+return {
+  -- amongst your other plugins
+  -- {'akinsho/toggleterm.nvim', version = "*", config = true}
+  -- or
+  {'akinsho/toggleterm.nvim', version = "*", opts = {
+		direction = 'float',
+		open_mapping = [[<A-i>]],
+		start_in_insert = true
+	}}
+}
